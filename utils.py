9dee02275780bea3afc679013cfc2f7e868022a3
sriram-ramanathan <32892642+sriram-ramanathan@users.noreply.github.com>
2019-08-21 18:38:19 +0530

    hit_rate module

---
+++ utils.py
@@ -0,0 +1,42 @@
+
+def find_hit_rates(train,dtype='string',level_threshold=90,sample_size=1000):
+    """"Finds the hitrate of variables of data type string or numeric"""
+    train=train.sample(sample_size)
+    black_cat=[]
+    black_level=[]
+    black_rates=[]
+    black_counts=[]
+    
+    if dtype=='string':
+        cats=list(train.dtypes[train.dtypes=='object'].index)
+    if dtype=='numeric':
+        cats=list(train.dtypes[train.dtypes!='object'].index)
+        
+    for cat in cats:
+        levels=train[cat].unique()
+        if len(levels)<=level_threshold:
+            #print(cat,'levels',levels)
+
+            for level in levels:
+                try:
+                    hitrate=sum(train[train[cat]==level]['isFraud'])/len(train[train[cat]==level])
+                    count=len(train[train[cat]==level])
+                   # print('cat',cat,' level:-',level,'hit rate',hitrate,count)
+                    #if hitrate > hit_threshold and count>count_threshold:
+
+                    black_cat.append(cat)
+                    black_level.append(level)
+                    black_rates.append(hitrate)
+                    black_counts.append(count)
+
+                except:
+                    #print('Exception')
+                    continue
+
+
+    hit_rate_df=pd.DataFrame(black_cat,columns=['Category'])
+    hit_rate_df['Level']=black_level
+    hit_rate_df['Hitrate']=black_rates
+    hit_rate_df['Counts']=black_counts
+    
+    return hit_rate_df
