d6d1e9fb41ec8d8d84d099193311cb49d40db3a9
sriram-ramanathan <32892642+sriram-ramanathan@users.noreply.github.com>
2019-08-21 18:50:41 +0530

    Added count threshold

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
+#non-numeric
 
-def find_hit_rates(train,dtype='string',level_threshold=90,sample_size=1000,hit_rate_threshold=0.80):
-    """"Finds the hitrate of variables of data type string or numeric and filters them for given threshold"""
+def find_hit_rates(train,dtype='string',num_level_threshold=90,sample_size=1000,hit_rate_threshold=0.80,count_threshold=100):
+    """"Finds the hitrate of variables of data type string or numeric and filters them for given thresholds"""
     train=train.sample(sample_size)
     black_cat=[]
     black_level=[]
@@ -14,7 +15,7 @@ def find_hit_rates(train,dtype='string',level_threshold=90,sample_size=1000,hit_
         
     for cat in cats:
         levels=train[cat].unique()
-        if len(levels)<=level_threshold:
+        if len(levels)<=num_level_threshold:
             #print(cat,'levels',levels)
 
             for level in levels:
@@ -39,6 +40,7 @@ def find_hit_rates(train,dtype='string',level_threshold=90,sample_size=1000,hit_
     hit_rate_df['Hitrate']=black_rates
     hit_rate_df['Counts']=black_counts
     hit_rate_df=hit_rate_df[hit_rate_df['Hitrate']>=hit_rate_threshold]
+    hit_rate_df=hit_rate_df[hit_rate_df['Counts']>=count_threshold]
     
     return hit_rate_df
 
